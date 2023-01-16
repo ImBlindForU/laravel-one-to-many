fa3cad22f62b272759120a7e6b875b4934074bcb
ImBlindForU <samuele.zanchi10@gmail.com>
2023-01-16 16:00:59 +0100

    first commit

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.admin')
+
+
+@section('content')
+    <div class="container">
+        <h2 class="text-center mt-3">crea un nuovo post</h2>
+        <div class="row justify-content-center">
+            <div class="col-8">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    
+                    </div>
+                @endif
+                <form action="{{ route('admin.project.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+
+                    
+
+                    <div class="form-group mb-3">
+                        <label for="title">Titolo</label>
+                        <input  type="text" name="title" id="title" class="form-control @error('title')
+                            is-invalid
+                        @enderror " value="{{ old('title')  }}">
+                        @error('title')
+                            <div class="invalid-feedback">Error</div>
+                            
+                        @enderror
+                    </div>
+                    {{-- img --}}
+                    <div class="form-group mb-3">
+                        <label for="cover_image">Immagine</label>
+                        <input type="file" id="cover_image" name="cover_image" class="form-control" @error('cover_image')
+                            is-invalid
+                        @enderror>
+                        @error('cover_image')
+                            {{$message}}
+                        @enderror
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="content">Contenuto</label>
+                        <textarea  value="{{ old('content')  }}" type="text" name="content" id="content" class="form-control" ></textarea>
+                    </div>
+
+                    <button class="btn btn-success" type="submit">Salva</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/projects/edit.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <h2 class="text-center mt-3">Modifica il titolo del progetto</h2>
+        <div class="row justify-content-center">
+            <div class="col-8">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    
+                    </div>
+                @endif
+                <form action="{{ route('admin.project.update',$project->slug) }}" method="POST" enctype="multipart/form-data">
+                    @method('PUT')
+                    @csrf
+
+                    <div class="form-group mb-3">
+                        <label for="title">Titolo</label>
+                        <input  type="text" name="title" id="title" class="form-control" value="{{$project->title}}">   
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="cover_image">Immagine</label>
+                        <input type="file" id="cover_image" name="cover_image" class="form-control">
+                         <div class="mt-4" id="image-preview">
+                            @if ($project->cover_image)
+                                <img class="w-50" src="{{asset('storage/'.$project->cover_image)}}" alt="">
+                            @else
+                                <p>nessuna immagine</p>
+                            @endif
+                         </div>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="content">Contenuto</label>
+                        <textarea  value="{{ old('content')  }}" type="text" name="content" id="content" class="form-control" >{{$project->content}}</textarea>
+                    </div>
+
+                    <button class="btn btn-warning" type="submit">Salva</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
